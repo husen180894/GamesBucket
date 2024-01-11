616c7727e1ec842a79614380295854065a9d5837
User <husen180893@gmail.com>
2024-01-11 10:45:16 +0700

    Google Fonts, iconsax, Rn Elements

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import {StatusBar} from 'react-native';
+import {Button} from '@rneui/themed';
+import {EmojiHappy} from 'iconsax-react-native';
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <View style={styles.container}>
-      <Text style={{color: '#234123'}}>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'#000'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Text style={styles.text}>Hello Developer</Text>
+        <Button>
+          <EmojiHappy color="#fff" variant="Broken" size={34} />
+        </Button>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -18,8 +27,18 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mainTitle: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 35,
+    fontWeight: '600',
+    fontFamily: 'Mulish',
+    color: '#342424',
   },
 });
